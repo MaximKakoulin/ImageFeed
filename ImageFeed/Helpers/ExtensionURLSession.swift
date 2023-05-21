0bf6c52bbe64825118e7be0151e8edf250833e32
MaximKakoulin <mximm1213@gmail.com>
2023-05-21 23:24:59 +0700

    Added new file Extension URLSession with generic

--- ImageFeed/Helpers/ExtensionURLSession.swift
+++ ImageFeed/Helpers/ExtensionURLSession.swift
@@ -5,4 +5,36 @@
 //  Created by Максим on 21.05.2023.
 //
 
-import Foundation
+import UIKit
+
+
+extension URLSession {
+    func objectTask<T:Decodable>(for request: URLRequest, completion: @escaping (Result<T, Error>) -> Void) -> URLSessionTask {
+        let fulfilCompletion: (Result<T, Error>) -> Void = { result in
+            DispatchQueue.main.async {
+                 completion(result)
+            }
+        }
+        let task = dataTask(with: request) { data, response, error in
+            if let data = data, let response = response as? HTTPURLResponse {
+                if 200..<300 ~= response.statusCode {
+                    let jsonDecoder = JSONDecoder()
+                    do {
+                        let decodedModel = try jsonDecoder.decode(T.self, from: data)
+                        fulfilCompletion(.success(decodedModel))
+                    } catch {
+                        fulfilCompletion(.failure(NetworkError.decodingError))
+                    }
+                } else {
+                    fulfilCompletion(.failure(NetworkError.httpStatusCode(response.statusCode)))
+                }
+            } else if let error {
+                fulfilCompletion(.failure(NetworkError.urlRequestError(error)))
+            } else {
+                fulfilCompletion(.failure(NetworkError.urlSessionError))
+            }
+        }
+        task.resume()
+        return task
+    }
+}
