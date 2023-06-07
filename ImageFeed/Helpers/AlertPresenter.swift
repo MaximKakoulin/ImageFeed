8c4d875109b6646f0b52ead20eb72dcdaf503ae5
MaximKakoulin <mximm1213@gmail.com>
2023-06-07 23:24:53 +0700

    recoded OAuth2

---
+++ ImageFeed/Helpers/AlertPresenter.swift
@@ -0,0 +1,48 @@
+//
+//  AlertPresenter.swift
+//  ImageFeed
+//
+//  Created by Максим on 07.06.2023.
+//
+
+import UIKit
+
+
+protocol AlertPresenterProtocol: AnyObject {
+    func show(result: AlertModel)
+    var delegate: UIViewController? {get set}
+}
+
+
+final class AlertPresenter: AlertPresenterProtocol {
+   weak var delegate: UIViewController?
+
+    func show(result: AlertModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.message ?? "",
+            preferredStyle: .alert)
+
+        let okAction = UIAlertAction(title: result.okButtonText, style: .default) { _ in
+            result.completion()
+        }
+        let noAction = UIAlertAction(title: result.noButtonText, style: .default) { _ in
+            result.completion()
+        }
+
+        alert.addAction(okAction)
+        alert.addAction(noAction)
+
+        delegate?.present(alert, animated: true, completion: nil)
+
+    }
+}
+
+
+struct AlertModel {
+    let title: String
+    let message: String?
+    let okButtonText: String
+    let noButtonText: String?
+    let completion: () -> () //замыкание без параметров для действия по кнопке аллерта
+}
